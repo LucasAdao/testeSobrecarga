483ad9448f7a881287a42d39a6d3591b080a8d9a
LucasAdao <l.adao@outlook.com>
2024-09-22 23:16:29 -0300

    Kamuii

---
+++ src/model/SharinganObito.java
@@ -0,0 +1,10 @@
+package model;
+
+public class SharinganObito {
+    public void kamui() {
+        System.out.println("O usuário se teleporta para outra dimensão e se torna inatingivel!");
+    }
+    public void kamui(String ninja){
+        System.out.println("O usuário manda o " + ninja + " para outra dimensão e ele fica lá pra sempre.");
+    }
+}
